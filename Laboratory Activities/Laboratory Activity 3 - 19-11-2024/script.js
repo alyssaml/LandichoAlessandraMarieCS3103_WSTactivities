4474a4659a67e1de1b20c327eca1ceb94c1a1e11
alyssaml <118249056+alyssaml@users.noreply.github.com>
2024-11-19 17:46:43 +0800

    Update script.js

--- Laboratory Activities/Laboratory Activity 3 - 19-11-2024/script.js
+++ Laboratory Activities/Laboratory Activity 3 - 19-11-2024/script.js
@@ -1,14 +1,11 @@
 const textInput = document.getElementById('textInput');
-    const wordCount = document.getElementById('wordcount');
-    const sentenceCount = document.getElementById('sentenceCount');
+const wordCount = document.getElementById('wordcount');
+const sentenceCount = document.getElementById('sentenceCount');
 
-    textInput.addEventListener('input', () => {
-      const text = textInput.value.trim();
-
-     
-      const words = text.length > 0 ? text.split(/\s+/).filter(word => word.length > 0) : [];
-      wordCount.textContent = `Words: ${words.length}`;
-
-      const sentences = text.length > 0 ? text.split(/[.!?]+/).filter(sentence => sentence.trim().length > 0) : [];
-      sentenceCount.textContent = `Sentences: ${sentences.length}`;
-    });
+textInput.addEventListener('input', () => {
+  const text = textInput.value.trim();
+  const words = text.length > 0 ? text.split(/\s+/).filter(word => word.length > 0) : [];
+  wordCount.textContent = `Words: ${words.length}`;
+  const sentences = text.length > 0 ? text.split(/[.!?]+/).filter(sentence => sentence.trim().length > 0) : [];
+  sentenceCount.textContent = `Sentences: ${sentences.length}`;
+});
